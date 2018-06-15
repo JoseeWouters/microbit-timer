2e810aec902a20cf33bc7c9a24dbd5345104d3b3
Josee <info@joseewouters.nl>
2018-06-15 21:26:09 +0200

    Add a short bleep when turning on leds and when timer is finished
    Thanks to the Mi:Power board, you can easily let the Micro:bit bleep.

--- timer.py
+++ timer.py
@@ -6,12 +6,16 @@ while True:
         for x in range(0, 5):
             for y in range(0, 5):
                 display.set_pixel(x,y,9)
+                pin0.write_digital(1) #make a bleep when turning on the leds
+                pin0.write_digital(0)
                 sleep(30000) #interval between putting on the leds
         display.clear()
         sleep(500)
         while True:
+            pin0.write_digital(1)
             display.show(Image.HAPPY)
             sleep(500)
+            pin0.write_digital(0)
             display.clear()
             sleep(500)
             if button_b.is_pressed(): #stop the end animation
